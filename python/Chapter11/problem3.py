bf1701354952a840950a1f38c9f3c05745f85eaf
ibrar11 <ibrarmunawar0gmail.com>
2025-10-16 12:52:00 +0500

    chapter 11 problem 3 done with increment calculation

--- python/Chapter11/problem3.py
+++ python/Chapter11/problem3.py
@@ -3,8 +3,8 @@ class Employee:
     def __init__(self, salary, increment):
         assert salary >= 0
         assert increment >= 0
-        self.__salary = salary
-        self.__increment = increment
+        self.setSalary = salary
+        self.setIncrement = increment
         
 
     @property
@@ -18,8 +18,18 @@ class Employee:
     @property
     def getIncrement(self):
         return self.__increment
+    
+    @getIncrement.setter
+    def setIncrement(self, increment):
+        self.__increment = increment
+    
+    @property
+    def salaryAfterIncrement(self):
+        return self.__salary + self.__salary * self.__increment
+
 
 
 emp1 = Employee(1000, 0.2)
 print(emp1.getSalary)
 print(emp1.getIncrement)
+print(emp1.salaryAfterIncrement)
